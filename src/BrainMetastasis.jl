a7975444376261b613081888a60c9ae37bc5a039
Ivan <solovev074@mail.ru>
2025-05-04 22:57:55 +0300

    stratify y

--- src/BrainMetastasis.jl
+++ src/BrainMetastasis.jl
@@ -3,10 +3,9 @@ module BrainMetastasis
     using Dates
     using CSV
     using Statistics
-    # using Plots, StatsPlots
+    using Plots, StatsPlots
     using MLJ
     using Logging
-    using StatisticalMeasures
     using StatisticalMeasuresBase
     using Shapley, CategoricalDistributions
     import Imbalance
@@ -33,17 +32,40 @@ module BrainMetastasis
                      :MetastasisTime])
     end
 
-    data = CSV.read("completed.csv", DataFrame)
-    dataRename!(data)
+    function cleanPreparing(balancing="no")
+        data = CSV.read("completed.csv", DataFrame)
+        dataRename!(data)
+        y, X = unpack(data, ==(:Progression), rng=1234)
+        y = coerce(y, OrderedFactor)
+        if(balancing == "oversampling")
+            println("Before oversamppling")
+            Imbalance.checkbalance(y)
+            oversampler = (@load RandomOversampler pkg=Imbalance verbosity=0)()
+            mach = machine(oversampler)
+            X, y = MLJ.transform(mach, X, y)
+            println("After oversamppling")
+        elseif(balancing == "SMOTE")
+            println("Before SMOTE")
+            Imbalance.checkbalance(y)
+            oversampler = (@load SMOTE pkg=Imbalance verbosity=0)()
+            mach = machine(oversampler)
+            X, y = MLJ.transform(mach, X, y)
+            println("After SMOTE")
+        elseif(balancing == "no")
+            println("No balancing")
+        end
+        Imbalance.checkbalance(y)
+        (Xtrain, Xtest), (ytrain, ytest) = partition((X, y), 0.8, rng=123, multi=true, stratify=y)
+        return Xtrain, Xtest, ytrain, ytest
+    end
 
-    function PreparingBestModels()
+    function PreparingBestModels(X, y)
         # X = DataFrames.select(data, Not(:"Интракраниальная прогрессия"))  # Все колонки, кроме 'target'
         # Y = DataFrames.select(data,"Интракраниальная прогрессия")              # Целевая переменная
 
         models = []
 
-        y, X = unpack(data, ==(:Progression), rng=1234)
-        y = coerce(y, OrderedFactor)
+        
         model = (@load KNNClassifier verbosity=0)()
         K_range = range(model, :K, lower=5, upper=20)
         self_tuning_knn = TunedModel(
@@ -56,7 +78,7 @@ module BrainMetastasis
         )
         mach = machine(self_tuning_knn, X, y)
         fit!(mach, verbosity=0)
-        push!(models, self_tuning_knn)
+        push!(models, mach)
 
         tree = (@load DecisionTreeClassifier pkg=DecisionTree verbosity=0)()
         m_depth_range = range(tree, :max_depth, lower=-1, upper=80);
@@ -97,5 +119,35 @@ module BrainMetastasis
         mach = machine(LogClass, X, y)
         fit!(mach, verbosity=0)
         push!(models, mach)
+        return models
+    end
+
+    function modelsReport(best_models, Xvalid, yvalid)
+        model_names = []
+        bac = []
+        jac = []
+        f2 = []
+        FN = []
+    
+        for model in best_models
+            try
+                push!(model_names, report(model).best_history_entry.model)
+            catch
+                push!(model_names, model.model)
+            end
+            y = predict_mode(model, Xvalid)
+            push!(bac, balanced_accuracy(y, yvalid))
+            push!(f2, FScore(beta = 2)(y, yvalid))
+            push!(FN, falsenegative(y, yvalid))
+        end
+    
+        df_valid = DataFrame(Models=model_names, balanced_acc=bac, F2=f2, FN=FN)
+        return df_valid
+    end
+
+    function main()
+        Xtrain, Xtest, ytrain, ytest = cleanPreparing("SMOTE")
+        models = PreparingBestModels(Xtrain, ytrain)
+        modelsReport(models, Xtest, ytest)
     end
 end # module BrainMetastasis
